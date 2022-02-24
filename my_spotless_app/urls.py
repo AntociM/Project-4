a4988ade63307d24fc94a83e37e8a88207a195cc
AntociM <gidioimihaela@gmail.com>
2022-02-24 19:44:19 +0000

    Create a user profile page

--- my_spotless_app/urls.py
+++ my_spotless_app/urls.py
@@ -1,8 +1,10 @@
 # from django.contrib import admin
 from django.urls import path
 from .views import home_page_view
+from .views import profile_view
 
 
 urlpatterns = [
     path('', home_page_view, name="home"),
+    path('profile', profile_view, name='profile')
 ]
--- my_spotless_app/views.py
+++ my_spotless_app/views.py
@@ -6,3 +6,9 @@ def home_page_view(request):
     context = {}
     return render(request, "index.html", context)
 
+def profile_view(request):
+    context = {
+        'user':request.user
+    }
+    return render(request, "profile.html", context)
+
