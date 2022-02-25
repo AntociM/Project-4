6a179ef301810604da15b09d907b2f0f0c5a6944
AntociM <gidioimihaela@gmail.com>
2022-02-25 14:57:26 +0000

    Create booking template and add url path

--- my_spotless_app/urls.py
+++ my_spotless_app/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import home_page_view
 from .views import edit_profile
 from .views import profile_dashboard_view
+from .views import booking_view
 
 
 urlpatterns = [
     path('', home_page_view, name='home'),
     path('profile/edit', edit_profile, name='edit_profile'),
     path('profile/dashboard', profile_dashboard_view, name='dashboard_profile'),
+    path('profile/booking', booking_view, name='booking'),
 ]
