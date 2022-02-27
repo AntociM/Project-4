66f0900d47c186b60def61b5c707bf35cfe5a074
AntociM <gidioimihaela@gmail.com>
2022-02-27 09:44:39 +0000

    Customize the admin page

--- my_spotless_app/admin.py
+++ my_spotless_app/admin.py
@@ -3,12 +3,23 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from .models import Service
+from .models import Contact
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['username', 'email',]
+    list_display = ['username', 'first_name', 'last_name', 'email',]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+
+@admin.register(Service)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'requisite']
+    ordering = ['name', 'price']
+    search_fields = ['name']
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'title', 'email', 'replied']
+    search_fields = ['name', 'title', 'email', 'telephone', 'message']
 
